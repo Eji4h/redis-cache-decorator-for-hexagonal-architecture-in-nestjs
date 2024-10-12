0013273d8ed3c4c353bb0337c2761c195b3c059f
Sikkapat Sricheangsa <bank.s.sricheangsa@gmail.com>
2024-10-12 16:55:52 +0700

    feat: 🎸 create repository to connect to item collection

--- src/items/adapters/items.adapter.module.ts
+++ src/items/adapters/items.adapter.module.ts
@@ -1,8 +1,17 @@
 import { Global, Module } from '@nestjs/common';
 import { ItemsV1Controller } from './inbounds/items.v1.controller';
+import { ItemsRepositoryToken } from '../applications/ports';
+import { ItemsMongoRepository } from './outbounds/repository/items.mongo.repository';
 
 @Global()
 @Module({
   controllers: [ItemsV1Controller],
+  providers: [
+    {
+      provide: ItemsRepositoryToken,
+      useClass: ItemsMongoRepository,
+    },
+  ],
+  exports: [ItemsRepositoryToken],
 })
 export class ItemsAdapterModule {}
