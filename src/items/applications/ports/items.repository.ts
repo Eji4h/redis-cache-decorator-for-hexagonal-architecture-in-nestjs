1e1684820044f2359d7182abaaec996a4ab6849e
Sikkapat Sricheangsa <bank.s.sricheangsa@gmail.com>
2024-10-12 15:57:05 +0700

    fix: 🐛 change create item parameter and add delete item to repo

--- src/items/applications/ports/items.repository.ts
+++ src/items/applications/ports/items.repository.ts
@@ -1,10 +1,11 @@
-import { IItem, ItemId } from '../../domains';
+import { IItem, ItemAttributes, ItemId } from '../../domains';
 
 export const ItemsRepositoryToken = Symbol('ItemsRepositoryToken');
 
 export interface ItemsRepository {
-  create(item: IItem): Promise<IItem>;
+  create(item: Omit<ItemAttributes, 'itemId'>): Promise<IItem>;
   findAll(): Promise<IItem[]>;
   findById(itemId: ItemId): Promise<IItem | undefined>;
   update(itemToUpdate: IItem): Promise<IItem>;
+  delete(itemId: ItemId): Promise<void>;
 }
